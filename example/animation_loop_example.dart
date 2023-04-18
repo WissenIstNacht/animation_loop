5fa420b36f7fb43c504a911d13e49d11f95a6329
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2023-04-18 10:55:09 +0200

    Feature: Add working example
    commit b6f88c25e440aae019d351f8da0a89441b143f42
    Author: WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
    Date:   Tue Apr 18 10:53:57 2023 +0200
    
        add styling
    
    commit d0b4f661e279d98c311d629fef86c3e6a152d9ee
    Author: WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
    Date:   Tue Apr 18 10:49:57 2023 +0200
    
        add simple counter example
    
    commit 158560d4406a0f1a5f699b47307cb1ddf8e4bffb
    Author: WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
    Date:   Tue Apr 18 10:10:27 2023 +0200
    
        add html stub
    
    commit 63e453f569adb56c596f683c7f0bf93658262748
    Author: WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
    Date:   Tue Apr 18 10:10:12 2023 +0200
    
        add web-app tooling

--- example/animation_loop_example.dart
+++ example/animation_loop_example.dart
@@ -1,10 +1,22 @@
+import 'dart:html';
+
 import 'package:animation_loop/animation_loop.dart';
 
 void main() {
-  var loop = AnimationLoop(.5);
-  loop.run(() => print('This print statement is executed every other second.'));
-  loop.stop();
+  var counterLoop = AnimationLoop(1);
+  var counter = 0;
 
-  var seconds = AnimationLoop(1);
-  loop.run(() => print('tick.'));
+  var btnCounterLoop = querySelector('#btnCounterLoop') as ButtonElement;
+  btnCounterLoop.addEventListener('click', (e) {
+    if (counterLoop.is_running) {
+      counterLoop.stop();
+      btnCounterLoop.text = 'Start';
+    } else {
+      counterLoop.run(() {
+        counter += 1;
+        querySelector('#counterDisplay').text = counter.toString();
+      });
+      btnCounterLoop.text = 'Stop';
+    }
+  });
 }
